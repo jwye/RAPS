0dde258d46d700291de1ebe4f7c8bfbbf1fa8730
JW Yeh <airwaveyjw@gmail.com>
2016-11-01 23:27:54 +0800

    no message

--- ReadPMsensor.py
+++ ReadPMsensor.py
@@ -1,13 +1,13 @@
-#form http://watchword.space/blog/?p=26
+#ref form http://watchword.space/blog/?p=26
 #encoding=utf-8
 import serial
 import time
 from struct import *
 
 # 打开串口
-print "Opening Serial Port..."
+print("Opening Serial Port...")
 ser = serial.Serial("/dev/ttyAMA0", 9600)
-print "Done"
+print("Done")
 try:
     cnt = 0
     while True:
@@ -17,13 +17,12 @@ try:
             # 读取内容并回显
             recv = ser.read(count)
             cnt = cnt + 1
-            print "[%d]Recieve Data" % cnt,
-            print len(recv), "Bytes:",
+            print()"[%d]Recieve Data" % cnt)
+            print(len(recv), "Bytes:")
             tmp = recv[4:16]
             datas = unpack('>hhhhhh', tmp)
-            print datas
-            print "insert into pm_log ('pm1','pm2_5','pm10') values (%d,%d,%d)" % (datas[0], datas[1],datas[2])
-
+            print(datas)
+            print("insert into pm_log ('pm1','pm2_5','pm10') values (%d,%d,%d)" % (datas[0], datas[1],datas[2]))
             #ser.write(recv)
             # 清空接收缓冲区
             ser.flushInput()
@@ -31,6 +30,6 @@ try:
         time.sleep(0.1)
 
 
-    except KeyboardInterrupt:
-        if ser != None:
-            ser.close()
+except KeyboardInterrupt:
+    if ser != None:
+        ser.close()
