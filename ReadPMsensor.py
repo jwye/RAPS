d23b16f9bf0b162f4405c6423da9399ad06e0f8f
JW Yeh <airwaveyjw@gmail.com>
2016-11-01 23:23:59 +0800

    no message

--- ReadPMsensor.py
+++ ReadPMsensor.py
@@ -3,15 +3,13 @@
 import serial
 import time
 from struct import *
-import sqlite3
+
 # 打开串口
 print "Opening Serial Port..."
 ser = serial.Serial("/dev/ttyAMA0", 9600)
 print "Done"
-def main():
+try:
     cnt = 0
-    conn = sqlite3.connect('pm25.db')
-    c = conn.cursor()
     while True:
         # 获得接收缓冲区字符
         count = ser.inWaiting()
@@ -24,19 +22,15 @@ def main():
             tmp = recv[4:16]
             datas = unpack('>hhhhhh', tmp)
             print datas
-            sql_str = """insert into pm_log ('pm1','pm2_5','pm10') values (%d,%d,%d)""" % (datas[0], datas[1],datas[2])
-            c.execute(sql_str)
-            conn.commit()
+            print "insert into pm_log ('pm1','pm2_5','pm10') values (%d,%d,%d)" % (datas[0], datas[1],datas[2])
+
             #ser.write(recv)
             # 清空接收缓冲区
             ser.flushInput()
         # 必要的软件延时
         time.sleep(0.1)
-    c.close()
-    conn.close()
-if __name__ == '__main__':
-    try:
-        main()
+
+
     except KeyboardInterrupt:
         if ser != None:
             ser.close()
